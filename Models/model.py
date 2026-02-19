01adfb379adf6794b10864bb0f01fedd7f784ddc
unplugged-aniimesh <sharmaanimesh14@gmail.com>
2026-02-19 11:40:59 +0530

    second

--- Models/model.py
+++ Models/model.py
@@ -1,33 +1,46 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
 
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    username=db.Column(db.String(20),unique=True,nullable=False)
-    email=db.Column(db.String(),unique=True,nullable=False)
-    password=db.Column(db.String(15),nullable=False)
-    
-    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    admin = db.Column(db.Boolean(), nullable=False)
+    user_cart = db.relationship("Cart", backref="User", lazy=True, cascade='all, delete-orphan')
+
+#either we can define here 
 class Category(db.Model):
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True) #-primary key
-    name=db.Column(db.String(20),unique=True,nullable=False)
-    products=db.relationship("Product",backref="Category",lazy=True,cascade="all,delete-orphan")
-    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    products = db.relationship("Product", backref="Category", lazy=True, cascade='all, delete-orphan')
+
+
 class Product(db.Model):
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    name=db.Column(db.String(20),unique=True,nullable=False)
-    price=db.Column(db.Integer,nullable=False)
-    category_id=db.Column(db.Integer,db.ForeignKey("category.id"),nullable=False)
-    
-     
-#cat=Category.query.first()
-#cat.products--
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    unit = db.Column(db.String(100), nullable=False)
+    mf_date = db.Column(db.Date, nullable=False)
+    expiry_date = db.Column(db.Date, nullable=False)
+    sold_quantity = db.Column(db.Integer, default=0, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
+  
 
 
-#pro=Product.query.first()
-#pro.category--
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    cart_count = db.Column(db.Integer, default=0, nullable=False)
+    items = db.relationship("CartItem", backref="Cart",cascade='all, delete-orphan')
 
 
-
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quantity = db.Column(db.Integer, default=0, nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey("cart.id"), nullable=False)
+    cartitem_product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+   
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+from sqlalchemy import or_
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from Models.model import *
-from flask import Flask,render_template,request,url_for
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
 
-app=Flask(__name__)
 
-
-app.config['SECRET_KEY']="East"
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///gs_store.sqlite3'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'East'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grocery_store.sqlite3'
 
 
 db.init_app(app)
@@ -16,6 +18,79 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+#admin credentials
+
+@app.route('/', methods=['GET'])
+def home():
+    return redirect(url_for('logout'))
+
+
+@app.route('/signup', methods=['GET',"POST"])
+def signup():
+    if request.method=="POST":
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        admin=False
+        
+        if request.form.get('admin_key')=="Asharma":
+            admin=True
+            
+            
+        try:
+            user=User(username=username,email=email,password=password,admin=admin)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+            
+        except IntegrityError: #we use for duplicate items
+            db.session.rollback()
+            flash("Username or email is already exists")
+            return redirect(url_for('signup'))
+        
+    return render_template('signup.html')
+        
+@app.route('/login', methods=['GET',"POST"])
+def login():
+    if request.method=="POST":
+        username_or_email=request.form['username']
+        password=request.form['password']   
+        
+        user=User.query.filter(
+            or_(User.username==username_or_email,User.email==username_or_email)).first()
+        
+        if user and user.password==password:
+            session['user_id']=user.id
+            return redirect(f'/dashboard/{user.id}')
+        
+        else:
+            error_msg="Invalid username or Password"
+            if not user:
+                error_msg="No user Found with provided username or email"
+            return render_template('login.html',error_msg=error_msg)
+        
+        
+    return render_template('login.html')        
+    
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+
+
+
+
+
+
+
+
+
 
 
 
