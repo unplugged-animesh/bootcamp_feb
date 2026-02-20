248aa58b20229382b9cf36f4fc132408d8d60167
unplugged-aniimesh <sharmaanimesh14@gmail.com>
2026-02-20 11:03:41 +0530

    crud

--- app.py
+++ app.py
@@ -74,6 +74,146 @@ def login():
         
     return render_template('login.html')        
     
+@app.route('/dashboard/<int:curr_login_id>' , methods=["GET"])
+def dashboard(curr_login_id):
+    if request.method=="GET":
+        if 'user_id' in session and session['user_id']==curr_login_id:
+            user =User.query.get(curr_login_id)
+            if user.admin:
+                return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id))
+            else:
+                return redirect(url_for('customer_dashboard',curr_login_id=curr_login_id))
+        flash("Please login to access the dashboard")
+        return redirect(url_for('logout'))
+  
+  
+  
+def get_user_admin(curr_login_id):
+    if 'user_id' in session and curr_login_id==session['user_id']:
+        user=User.query.get(curr_login_id)
+        return user.admin
+    return False
+      
+    
+@app.route('/admin/<int:curr_login_id>/create_category' ,methods=["GET","POST"])
+def create_category(curr_login_id):
+    if not get_user_admin(curr_login_id):
+        flash("You are not authorised to see this page")
+        return redirect(url_for('logout'))
+    
+    if request.method=="POST":
+        name=request.form['name']
+        
+        try:
+            category=Category(name=name)
+            db.session.add(category)
+            db.session.commit()
+            return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id))
+      
+        except IntegrityError:
+            db.session.rollback()
+            flash("This category is already exists")
+            return redirect(url_for('create_catgory',curr_login_id=curr_login_id))
+        
+    return render_template('create_category.html',curr_login_id=curr_login_id)
+    
+    
+    
+@app.route('/admin/<int:curr_login_id>/edit_category/<int:category_id>' ,methods=["GET","POST"])
+def edit_category(curr_login_id,category_id):
+    if not get_user_admin(curr_login_id):
+        flash("You are not authorised to see this page")
+        return redirect(url_for('logout'))
+    
+    
+    category=Category.query.get_or_404(category_id)
+    
+    if request.method=="POST":
+        try:
+            category.name=request.form['name']
+            db.session.commit()
+            return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id,category_id=category.id))
+      
+        except IntegrityError:
+            db.session.rollback()
+            flash("This category is already exists")
+            return redirect(url_for('edit_catgory',curr_login_id=curr_login_id,category_id=category.id))
+        
+    return render_template('edit_category.html',curr_login_id=curr_login_id,category=category)
+
+
+       
+@app.route('/admin/<int:curr_login_id>/remove_category/<int:category_id>' ,methods=["GET","POST"])
+def remove_category(curr_login_id,category_id):
+    if not get_user_admin(curr_login_id):
+        flash("You are not authorised to see this page")
+        return redirect(url_for('logout'))
+     
+     
+    category=Category.query.get_or_404(category_id)
+    
+    if request.method=="POST":
+        db.session.delete(category)
+        db.session.commit()
+        return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id,category=category))
+    
+    return render_template('remove_category.html',curr_login_id=curr_login_id,category=category)
+
+    
+@app.route('/admin/<int:curr_login_id>/dashboard',methods=["GET"])
+def admin_dashboard(curr_login_id):
+    if request.method=="GET":
+        if 'user_id' in session and session['user_id']==curr_login_id:
+            user =User.query.get(curr_login_id)
+            if not user.admin:
+                flash("You are not authorised to see this page")
+                return redirect(f'/dashboard/{curr_login_id}')
+            
+            
+            categories=Category.query.all()
+            data={'curr_login_id':curr_login_id,
+                  'categories': categories}
+            
+            return render_template('admin_dashboard.html', data=data,name=User.query.get(curr_login_id).username)
+            
+            
+            
+            
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
